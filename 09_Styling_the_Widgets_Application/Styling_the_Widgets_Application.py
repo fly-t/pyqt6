017aea018aa0e9ba2a357efe6e27b980d8f47c80
fly-t <fly-t@163.com>
2023-11-26 15:15:56 +0800

    set style

--- 09_Styling_the_Widgets_Application/Styling_the_Widgets_Application.py
+++ 09_Styling_the_Widgets_Application/Styling_the_Widgets_Application.py
@@ -6,5 +6,11 @@ if __name__ == "__main__":
     app = QApplication()
     w = QLabel("This is a placeholder text")
     w.setAlignment(Qt.AlignCenter)
+    w.setStyleSheet("""
+        background-color: #262626;
+        color: #FFFFFF;
+        font-family: Titillium;
+        font-size: 18px;
+        """)
     w.show()
     sys.exit(app.exec())
